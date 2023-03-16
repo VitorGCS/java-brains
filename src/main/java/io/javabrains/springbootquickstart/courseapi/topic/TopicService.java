9992468db3121111fae4d337109ad699c1825ace
vitor <vgc_silva@hotmail.com>
2023-03-16 11:57:29 +0000

    Adding repository and configure entity topic and update service to use repository

--- src/main/java/io/javabrains/springbootquickstart/courseapi/topic/TopicService.java
+++ src/main/java/io/javabrains/springbootquickstart/courseapi/topic/TopicService.java
@@ -1,5 +1,6 @@
 package io.javabrains.springbootquickstart.courseapi.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,35 +10,48 @@ import java.util.List;
 @Service
 public class TopicService {
 
-    private List<Topic> topics = new ArrayList<>(Arrays.asList(
+    @Autowired
+    private TopicRepository topicRepository;
+
+
+/*    private List<Topic> topics = new ArrayList<>(Arrays.asList(
             new Topic("spring", "Spring Framework", "Spring Framework Description"),
             new Topic("java", "Core Java", "Core Java Description"),
             new Topic("javaScript", "JavaScript", "JavaScript Description")
-    ));
+    ));*/
 
     public List<Topic> getAllTopics(){
+//        return topics;
+        List<Topic> topics = new ArrayList<>();
+        topicRepository.findAll().forEach( topics::add);
         return topics;
     }
 
+
     public Topic getTopic(String id) {
-        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+//        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+        return topicRepository.findById(id).get();
+
     }
 
     public void addTopic(Topic newTopic) {
-        topics.add(newTopic);
+//        topics.add(newTopic);
+        topicRepository.save(newTopic);
     }
 
     public void updateTopic(String id, Topic topic) {
-        for(int i = 0; i< topics.size(); i++){
+       /* for(int i = 0; i< topics.size(); i++){
             Topic t = topics.get(i);
             if(t.getId().equals(id)){
                 topics.set(i, topic);
                 return;
             }
-        }
+        }*/
+        topicRepository.save(topic);
     }
 
     public void deleteTopic(String id) {
-        topics.removeIf( t -> t.getId().equals(id));
+//        topics.removeIf( t -> t.getId().equals(id));
+        topicRepository.deleteById(id);
     }
 }
